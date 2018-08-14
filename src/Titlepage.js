3a55ee0f7cd80646c8ce27cc7a27d79180ab6b1e
Jonathan Hinton <jonathanhinton@Jonathans-MacBook-Pro.local>
2018-08-14 13:52:13 -0500

    remove clutter from titlepage

--- src/Titlepage.js
+++ src/Titlepage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 
 const Titlepage = ({myText}) => {
-    //const chapter = myText.book.content[currPosition];
 
     return(
         <h1 className="App-title">{myText}</h1>
